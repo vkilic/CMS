d1d200ad23ebfd267d789e6e093c9c8f66427641
Volkan Kılıç <vlknklc902@gmail.com>
2014-07-11 18:03:28 +0300

    cms
    İçerik Yönetim Sistemi

---
+++ baslik.php
@@ -0,0 +1,155 @@
+<meta http-equiv="Content-Type" content="text/HTML; charset=utf-8">
+
+
+
+ <!-- UST MENU BAŞLANGIÇ -->
+        <div id="topbar">
+            <div class="container">
+                <div class="row">
+                    <div id="nav" class="span12 light">
+
+                        <!-- START MAIN NAVIGATION -->
+
+                        <ul id="menu-menu" class="level-1">
+                            <li>
+                                <a href="index.html">Anasayfa</a>
+                                
+                            </li>
+                            <li>
+                                <a href="#">Akademik</a>
+                                <ul class="sub-menu">
+                                    <li><a href="#">Revolution Slider</a></li>
+                                    <li><a href="#">Flexslider Classic</a></li>
+                                    <li><a href="#">Flexslider Elegant</a></li>
+                                    <li><a href="#">Elastic</a></li>
+                                    <li><a href="#">Flash</a></li>
+                                    <li><a href="#">Rotating</a></li>
+                                    <li><a href="#">Thumbnails</a></li>
+                                    <li><a href="#">Usquare</a></li>
+                                    <li><a href="#">Polaroid</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">FEATURES</a>
+                                <ul class="sub-menu">
+                                    <li><a href="services-module.html">Services Module</a></li>
+                                    <li><a href="blog-modules.html">Blog modules</a></li>
+                                    <li><a href="portfolio-and-video-modules.html">Portfolio and video modules</a></li>
+                                    <li><a href="libra-features.html">Libra features</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">PORTFOLIO</a>
+                                <ul class="sub-menu">
+                                    <li><a href="portfolio-libra.html">Libra</a></li>
+                                    <li><a href="portfolio-pinterest.html">Pinterest</a></li>
+                                    <li><a href="portfolio-slide-detail.html">Slide Detail</a></li>
+                                    <li><a href="portfolio-filterable.html">Filterable</a></li>
+                                    <li><a href="portfolio-2-columns.html">2 Columns</a></li>
+                                    <li><a href="portfolio-3-columns.html">3 Columns</a></li>
+                                    <li><a href="portfolio-4-columns.html">4 Columns</a></li>
+                                    <li><a href="portfolio-big-image.html">Big Image</a></li>
+                                    <li><a href="portfolio-slider.html">Slider</a></li>
+                                    <li><a href="portfolio-project-detail-1.html">Project Detail #1</a></li>
+                                    <li><a href="portfolio-project-detail-2.html">Project detail #2</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">PAGES</a>
+                                <ul class="sub-menu">
+                                    <li>
+                                        <a href="testimonials.html">Testimonials</a></li>
+                                    <li>
+                                        <a href="#">Blog</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="blog-libra-big.html">Libra Big</a></li>
+                                            <li><a href="blog-libra-small.html">Libra Small</a></li>
+                                            <li><a href="blog-elegant.html">Elegant</a></li>
+                                            <li><a href="blog-big-thumbnails.html">Big Thumbnails</a></li>
+                                            <li><a href="blog-small-thumbnails.html">Small Thumbnails</a></li>
+                                            <li><a href="blog-pinterest.html">Pinterest</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="pages-faq.html">FAQ</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">About</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="accordion-style.html">Accordion Style</a></li>
+                                            <li><a href="circle-style.html">Circle Style</a></li>
+                                            <li><a href="u-square-style.html">U-square style</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="error-404.html">Error 404</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">SHORTCODES</a>
+                                <ul class="sub-menu">
+                                    <li><a href="shortcodes-alert-box-buttons.html">Alert box &amp; Buttons</a></li>
+                                    <li><a href="shortcodes-charts.html">Charts</a></li>
+                                    <li><a href="shortcodes-icon-section.html">Icon section</a></li>
+                                    <li><a href="shortcodes-media-widgets.html">Media &amp; Widgets</a></li>
+                                    <li><a href="shortcodes-mix-various.html">Mix &amp; Various</a></li>
+                                    <li><a href="shortcodes-table-box-prices.html">Table &amp; Box prices</a></li>
+                                    <li><a href="shortcodes-typography.html">Typography</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">CONTACT</a>
+                                <ul class="sub-menu">
+                                    <li><a href="get-in-touch.html">With MAP</a></li>
+                                    <li><a href="get-in-touch-2.html">Without MAP</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <!-- END MAIN NAVIGATION -->
+
+                        <!-- START TOPBAR LOGIN -->
+
+                        <div id="topbar_login" class="not_logged_in">
+
+                            <a class="topbar_login" href="#">
+                                Giriş <span class="sf-sub-indicator"></span>
+                            </a>
+
+                            <div id="fast-login" class="access-info-box">
+                                <form action="#" method="post" name="loginform">
+
+                                    <div class="form">
+                                        <p>
+                                            <label>
+                                                Kullanıcı Adı<br/>
+                                                <input type="text" tabindex="10" size="20" value="" name="log" class="input-text" />
+                                            </label>
+                                        </p>
+
+                                        <p>
+                                            <label>
+                                                Şifre<br/>
+                                                <input type="password" tabindex="20" size="20" value="" name="pwd" class="input-text" />
+                                            </label>
+                                        </p>
+
+                                        <a class="align-left lostpassword" href="#" title="Password Lost and Found">
+                                            Parolamı Unuttum
+                                        </a>
+
+                                        <p class="align-right">
+                                            <input type="submit" tabindex="100" value="Giriş" name="wp-submit" class="input-submit" />
+                                            <input type="hidden" value="index.html" name="redirect_to" />
+                                            <input type="hidden" value="1" name="testcookie" />
+                                        </p>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <!-- END TOPBAR LOGIN -->
+                    </div>
+                </div>
+            </div>
+        </div>
+         <!-- UST MENU BİTİŞ -->
